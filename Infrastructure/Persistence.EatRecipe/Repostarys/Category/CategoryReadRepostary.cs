10da7f229b9c5525ccb03ae8e3a6298f17866571
Terlan Imanov <imanov548@gmail.com>
2024-05-17 11:46:54 +0400

    reload

--- Infrastructure/Persistence.EatRecipe/Repostarys/Category/CategoryReadRepostary.cs
+++ Infrastructure/Persistence.EatRecipe/Repostarys/Category/CategoryReadRepostary.cs
@@ -1,11 +1,6 @@
 ﻿using Appilacation.EatRecipe.Repostarys;
 using Domen.EatRecipe.Entitys;
 using Persistence.EatRecipe.Context;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Persistence.EatRecipe.Repostarys
 {
